568777106ac09debd54de72215bf12473aea59e3
GiorgiBeqauri <gio.beqauri.02@gmail.com>
2025-04-11 23:28:33 +0400

    commit

---
+++ Day 008/claswork/claswork.py
@@ -0,0 +1,30 @@
+# 1) მომხმარებლისგან ორი რიცხვის მიღება:
+num1 = int(input("შეიყვანეთ პირველი რიცხვი: "))
+num2 = int(input("შეიყვანეთ მეორე რიცხვი: "))
+
+# 2) შედარების ოპერაციები:
+# მეტობა
+print(num1 > num2)   # True თუ num1 მეტია num2-ზე, False წინააღმდეგ შემთხვევაში
+# ნაკლობა
+print(num1 < num2)   # True თუ num1 ნაკლებია num2-ზე, False წინააღმდეგ შემთხვევაში
+# ტოლობა
+print(num1 == num2)  # True თუ num1 ტოლია num2-სთან, False წინააღმდეგ შემთხვევაში
+# არათოლობა
+print(num1 != num2)  # True თუ num1 არ არის ტოლი num2-ს, False წინააღმდეგ შემთხვევაში
+# მეტია ან ტოლი
+print(num1 >= num2)  # True თუ num1 მეტია ან ტოლია num2-ს, False წინააღმდეგ შემთხვევაში
+# ნაკლები ან ტოლი
+print(num1 <= num2)  # True თუ num1 ნაკლებია ან ტოლია num2-ს, False წინააღმდეგ შემთხვევაში
+
+# 3) 5 True და 5 False
+print(num1 > num2)   # True
+print(num1 < num2)   # False
+print(num1 == num2)  # False
+print(num1 != num2)  # True
+print(num1 >= num2)  # False
+print(num1 <= num2)  # True
+print(num1 > num2)   # True
+print(num1 < num2)   # False
+print(num1 == num2)  # False
+print(num1 != num2)  # True
+﻿
